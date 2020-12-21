92a58a81caf1f0466947e1a795dc3e51b06b98aa
Casey Arnold <caseyarnold@Caseys-MacBook-Pro.local>
2020-12-21 16:24:00 -0500

    added navigation, photo of myself

--- caseyarnold.js
+++ caseyarnold.js
@@ -1,1 +1,19 @@
 const gp = gsap.timeline( { defaults: { ease: 'power1.out' } })
+
+document.querySelector('#navigation-trigger').addEventListener('click', function () {
+  gp.to('#navigation-screen', {
+    opacity: 1,
+    zIndex: 500,
+    duration: 0.5,
+    ease: 'easeIn'
+  })
+})
+
+document.querySelector('#exit-button img').addEventListener('click', function () {
+  gp.to('#navigation-screen', {
+    opacity: 0,
+    zIndex: -500,
+    duration: 0.5,
+    ease: 'easeOut'
+  })
+})
