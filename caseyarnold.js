2687ef314fc5a40b1450d570eb93bce0a0180d98
Casey Arnold <casey.arnold@me.com>
2020-12-22 16:59:36 -0500

    converted animations from gsap to jquery

--- caseyarnold.js
+++ caseyarnold.js
@@ -1,32 +1,21 @@
-const gp = gsap.timeline({ defaults: { ease: 'power1.out' } })
-
-document.querySelector('#navigation-trigger').addEventListener('click', function () {
-  showScreen('#navigation-screen')
+$('#navigation').click(function () {
+  $(this).hide()
+  $("#navigation-screen").fadeIn("slow", function () {})
+  $("#exit-button").show()
 })
 
-document.querySelector('#exit-button img').addEventListener('click', function () {
-  hideScreen('.hidden')
+$('#exit-button').click(function () {
+  hideWindows()
 })
 
-document.querySelector('#about-trigger').addEventListener('click', function () {
-  hideScreen('.hidden')
-  showScreen('#about-screen')
+$("#about-trigger").click(function () {
+  hideWindows()
+  $("#about-screen").fadeIn("slow", function () {})
+  $("#navigation").show(function() { alert(5) })
 })
 
-function hideScreen (id) {
-  gp.to(id, {
-    opacity: 0,
-    zIndex: -500,
-    duration: 0.5,
-    ease: 'easeOut'
-  })
-}
-
-function showScreen (id) {
-  gp.to(id, {
-    opacity: 1,
-    zIndex: 500,
-    duration: 0.5,
-    ease: 'easeIn'
-  })
+function hideWindows () {
+  $('#exit-button').hide()
+  $(".hidden").fadeOut()
+  $("#navigation").show()
 }
