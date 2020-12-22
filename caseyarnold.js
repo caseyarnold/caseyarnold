b543faf9fe7c90d1220acdfacb16fc65a8de3103
Casey Arnold <caseyarnold@Caseys-MacBook-Pro.local>
2020-12-22 14:47:18 -0500

    Added some about screen functionality

--- caseyarnold.js
+++ caseyarnold.js
@@ -1,19 +1,32 @@
 const gp = gsap.timeline({ defaults: { ease: 'power1.out' } })
 
 document.querySelector('#navigation-trigger').addEventListener('click', function () {
-  gp.to('#navigation-screen', {
-    opacity: 1,
-    zIndex: 500,
-    duration: 0.5,
-    ease: 'easeIn'
-  })
+  showScreen('#navigation-screen')
 })
 
 document.querySelector('#exit-button img').addEventListener('click', function () {
-  gp.to('#navigation-screen', {
+  hideScreen('.hidden')
+})
+
+document.querySelector('#about-trigger').addEventListener('click', function () {
+  hideScreen('.hidden')
+  showScreen('#about-screen')
+})
+
+function hideScreen (id) {
+  gp.to(id, {
     opacity: 0,
     zIndex: -500,
     duration: 0.5,
     ease: 'easeOut'
   })
-})
+}
+
+function showScreen (id) {
+  gp.to(id, {
+    opacity: 1,
+    zIndex: 500,
+    duration: 0.5,
+    ease: 'easeIn'
+  })
+}
