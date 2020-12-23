a13436af886121844035fb60a860cd88cf47a8b5
Casey Arnold <casey.arnold@me.com>
2020-12-22 20:21:35 -0500

    added design elements

--- caseyarnold.js
+++ caseyarnold.js
@@ -1,21 +1,26 @@
 $('#navigation').click(function () {
   $(this).hide()
-  $("#navigation-screen").fadeIn("slow", function () {})
-  $("#exit-button").show()
+  $('#navigation-screen').fadeIn('slow', function () {})
+  $('#exit-button').show()
 })
 
 $('#exit-button').click(function () {
   hideWindows()
 })
 
-$("#about-trigger").click(function () {
+$('#about-trigger').click(function () {
   hideWindows()
-  $("#about-screen").fadeIn("slow", function () {})
-  $("#navigation").show(function() { alert(5) })
+  $('#about-screen').fadeIn('slow', function () {})
+  $('#navigation').show(function() { })
+})
+
+$('#main-blurb').click(function () {
+  // show portfolio page
+  alert('Eventually will show portfolio page')
 })
 
 function hideWindows () {
   $('#exit-button').hide()
-  $(".hidden").fadeOut()
-  $("#navigation").show()
+  $('.hidden').fadeOut()
+  $('#navigation').show()
 }
